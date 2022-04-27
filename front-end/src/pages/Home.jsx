780c935951243e7892cc70f07a8188bfc923521b
Matheus <ms.conrado25@hotmail.com>
2022-04-27 19:08:10 -0300

    fix: lint

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function Home() {
   return (
     <div>
-      <button>Botão</button>
+      {/* <button>Botão</button> */}
     </div>
   );
 }
