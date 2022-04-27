ac6ae54e7872e28c51392720e376086fe57f9b8f
Matheus <ms.conrado25@hotmail.com>
2022-04-27 16:37:20 -0300

    feat: criando o models, a migrations e o seeders dos users

---
+++ back-end/src/database/seeders/20220427190916-users.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    queryInterface.bulkInsert("users", [
+      {
+        name: "Delivery App Admin",
+        email: "adm@deliveryapp.com",
+        password: "a4c86edecc5aee06eff8fdeda69e0d04",
+        role: "administrator",
+        createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      {
+        name: "Fulana Pereira",
+        email: "fulana@deliveryapp.com",
+        password: "3c28d2b0881bf46457a853e0b07531c6",
+        role: "seller",
+        createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      {
+        name: "Cliente Zé Birita",
+        email: "zebirita@email.com",
+        password: "1c37466c159755ce1fa181bd247cb925",
+        role: "customer",
+        createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    ]);
+  },
+
+  async down(queryInterface, _Sequelize) {
+    queryInterface.bulkDelete("users", null, {});
+  },
+};
