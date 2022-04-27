0354a95278a6d775e2ce89ba15df7bfaa018b2c2
brunofeu <brunofeu89@gmail.com>
2022-04-27 18:21:21 -0300

    change input to password

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 function Form() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const EMAIL_VALIDATION = /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+  const { register, handleSubmit, watch, formState: { errors, isValid } } = useForm({
+    mode: "onChange"
+  });
 
-  const onSubmit = data => console.log(data);
+  const onSubmit = data => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>
         Email
         <input
-          {...register("email")}
-          placeholder="email@trybeer.com.br"
+          {...register("email", { required: true, pattern: {
+            value: EMAIL_VALIDATION,
+            message: 'Invalid email'
+          } })}
+          placeholder="email@trybe.com.br"
           data-testid="common_login__input-email"
         />
       </label>
       <label>
         Senha
         <input
-          {...register("password")}
+          type="password"
+          {...register("password", { required: true, minLength: 6  })}
           placeholder="*******"
           data-testid="common_login__input-password"
         />
@@ -28,17 +35,22 @@ function Form() {
         <button
           type="submit"
           data-testid="common_login__button-login"
+          disabled={!isValid}
         >
           Login
         </button>
         <button
+          type="button"
           data-testid="common_login__button-register"
         >
           Ainda não tenho conta
         </button>
       </div>
       <div data-testid="common_login__element-invalid-email">
-        {errors.exampleRequired && <span>This field is required</span>}
+        {/* { errors.email?.message && <span>Email is invalid</span>}
+        { errors.password?.type === "minLength" && <span>Password is less than 6 characters</span>}
+        { errors.password?.type === "required" && <span>Password is required</span>}
+        { errors.email?.type === "required" && <span>Email is required</span>} */}
       </div>
     </form>
   )
