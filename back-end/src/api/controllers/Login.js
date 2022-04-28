ede56502b743d6d50dfba6cf135ce5c89bfc3a5b
Matheus <ms.conrado25@hotmail.com>
2022-04-28 11:57:53 -0300

    feat: adicionando funcionalidade md5

--- back-end/src/api/controllers/Login.js
+++ back-end/src/api/controllers/Login.js
@@ -1,9 +1,10 @@
+const md5 = require('md5');
 const services = require('../services/Login');
 
 const getLogin = async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await services.getLogin(email, password);
+  const user = await services.getLogin(email, md5(password));
 
   return res.status(user.status).json(user.data);
 };
