50eff87a73811a0abed9903e6c6d0d3c17ba6f3f
Matheus <ms.conrado25@hotmail.com>
2022-04-27 23:05:10 -0300

    feat: adicionando o cors, e o router

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors');
+const router = require("./routes/routes");
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(cors())
+app.use(express.json());
+app.use('/', router);
+
+app.get("/coffee", (_req, res) => res.status(418).end());
 
 module.exports = app;
