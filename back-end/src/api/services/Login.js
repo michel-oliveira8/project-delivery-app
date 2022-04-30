5b4e7be429e035e6509164d3fad34e5d36fa165c
Matheus <ms.conrado25@hotmail.com>
2022-04-30 10:18:50 -0300

    fix: fazendo pequenas alteração no login para criar o token qunado o cliente logar

--- back-end/src/api/services/Login.js
+++ back-end/src/api/services/Login.js
@@ -1,17 +1,27 @@
 const { user } = require('../../database/models');
+const createToken = require('../../token/createToken');
 
 const getLogin = async (email, password) => {
-  const res = await user.findOne({ where: { email, password } });
+  const res = await user.findOne({ 
+    where: { email, password }, 
+    attributes: ['name', 'email', 'role'],
+  });
+
   if (!res) {
     return {
       status: 404,
-      data: 'Not found',
+      data: { error: 'Not found' },
     };
   }
 
+  const token = await createToken({ name: res.name, email, role: res.role });
+
   return {
     status: 200,
-    data: res.dataValues,
+    data: {
+      ...res.dataValues,
+      token,
+    },
   };
 };
 
