351cd6662343a1651401e374864b017e13d31fa0
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-04-29 21:09:16 -0300

    insere rota /register

--- back-end/src/api/routes/routes.js
+++ back-end/src/api/routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getLogin } = require('../controllers/Login');
+const { create } = require('../controllers/Register');
 
 const router = express.Router();
 
 router.post('/login', getLogin);
+router.post('/register', create);
 
 module.exports = router;
