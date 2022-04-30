b34ed1952eda613a3c0aeb8c14dc4bf8d2f09bc0
Matheus <ms.conrado25@hotmail.com>
2022-04-30 10:19:23 -0300

    fix: alterando a resposta da message de error

--- back-end/src/api/services/Register.js
+++ back-end/src/api/services/Register.js
@@ -4,7 +4,7 @@ const create = async (name, email, password, role) => {
   const userExist = await user.findOne({ where: { name, email } });
 
   if (userExist) {
-    return { status: 409, message: 'User exist' }; 
+    return { status: 409, message: { error: 'User exist' } }; 
   }
 
   const createUser = await user.create({ name, email, password, role });
