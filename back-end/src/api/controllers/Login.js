bfd586a43dfbc31475e8fd19e2b35c85a33e5aa5
Matheus <ms.conrado25@hotmail.com>
2022-05-02 15:54:21 -0300

    fix:  add try catch

--- back-end/src/api/controllers/Login.js
+++ back-end/src/api/controllers/Login.js
@@ -2,11 +2,15 @@ const md5 = require('md5');
 const services = require('../services/Login');
 
 const getLogin = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await services.getLogin(email, md5(password));
-
-  return res.status(user.status).json(user.data);
+  try {
+    const { email, password } = req.body;
+  
+    const user = await services.getLogin(email, md5(password));
+  
+    return res.status(user.status).json(user.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
