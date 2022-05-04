4b23852ff9369f54a3b090a3bbbfb114cdd692c0
brunofeu <brunofeu89@gmail.com>
2022-05-04 19:33:31 -0300

    style: remove createdAt e updatedAt da tabela sales

--- back-end/src/database/migrations/20220427194416-create-sales.js
+++ back-end/src/database/migrations/20220427194416-create-sales.js
@@ -56,14 +56,6 @@ module.exports = {
         type: Sequelize.STRING,
         
       },
-      // createdAt: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE,
-      // },
-      // updatedAt: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE,
-      // },
     });
   },
   async down(queryInterface, Sequelize) {
