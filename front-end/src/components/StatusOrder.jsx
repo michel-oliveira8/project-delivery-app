5436f7414132a7bc96af05604d797d1104cd3435
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-05-06 15:33:20 -0300

    componente statusOrder

--- front-end/src/components/StatusOrder.jsx
+++ front-end/src/components/StatusOrder.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function StatusOrder() {
+  const [order, setOrder] = useState([]);
+
+
+  useEffect(() => {
+    const userId  = JSON.parse(localStorage.getItem('user'))
+    console.log(userId);
+    // axios.get('http://localhost:3001/order/${userId.id}')
+    // .then(() => )
+    // .catch(() => );
+  }, []);
+
+
   return (
     <div>
-      <p>Funcionou</p>
+      {order.map(({ userId, status, saleDate, totalPrice }) => (
+      <button
+        type="button"
+        key={ userId }
+      >
+        <table>
+          <tr>
+            <td
+              data-testid={ `customer_orders__element-order-${userId}` }
+            >
+              { userId  }
+            </td>
+            <td
+              data-testid={`customer_orders__element-delivery-status-${userId}`}
+              type="button"
+            >
+              { status }
+            </td>
+            <td
+              data-testid={`customer_orders__element-order-date-${userId}`}
+            >
+              { saleDate }
+            </td>
+            <td>
+              { totalPrice }
+            </td>
+          </tr>
+        </table>
+
+      </button>
+      ))}
     </div>
   );
 }
